664b68e58fdab0d7c73e96757e39eb8e6a9bd37d
Lorna Lynch <LornaLynch@users.noreply.github.com>
2015-08-25 12:24:53 +0100

    Change Astro Pi to Sense HAT

--- code/acceleration.py
+++ code/acceleration.py
@@ -1,9 +1,9 @@
-from astro_pi import AstroPi
-ap = AstroPi()
+from sense_hat import SenseHat
+sense = SenseHat()
 import time
 
 while True:
-  x,y,z = ap.get_accelerometer_raw().values()
+  x,y,z = sense.get_accelerometer_raw().values()
 
   x=round(x,0)
   y=round(y,0)
