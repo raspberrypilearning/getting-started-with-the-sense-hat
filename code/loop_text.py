93fd10e72ceff0c98831febc7a62e762d96fa86b
James Robinson <james.robinson@raspberrypi.org>
2015-09-14 11:49:46 +0100

    Update loop_text.py

--- code/loop_text.py
+++ code/loop_text.py
@@ -1,6 +1,6 @@
-from astro_pi import AstroPi
+from sense_hat import SenseHat
 
-ap = AstroPi()
+sense = SenseHat()
 
 while True:
     ap.show_message("Astro Pi is awesome!!", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255])
